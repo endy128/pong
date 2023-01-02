b8f514c5d87a31a046cf05adbaa3e1eb00986013
Neil Douglas <endy64@icloud.com>
2023-01-02 14:30:16 +0000

    paddles move

--- script.js
+++ script.js
@@ -7,24 +7,24 @@ let ctx = canvas.getContext('2d')
 
 const paddleLength = 150
 const paddleWidth = 10
-const paddleSpeed = 20
+const paddleSpeed = 6
 
 window.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'q':
-      player1.self.update(-paddleSpeed)
+      player1.self.up()
       break
     case 'a':
-      player1.self.update(paddleSpeed)
+      player1.self.down()
       break
     case 'p':
-      player2.self.update(-paddleSpeed)
+      player2.self.up()
       break
     case 'l':
-      player2.self.update(paddleSpeed)
+      player2.self.down()
       break
-
     default:
+      player1.self.stop()
       break
   }
 })
@@ -33,13 +33,24 @@ const paddleFactory = ({ x, y }) => {
   let self = {
     x,
     y,
+    velocity: 0,
+    up: () => {
+      self.velocity = -paddleSpeed
+    },
+    down: () => {
+      self.velocity = paddleSpeed
+    },
+    stop: () => {
+      self.velocity = 0
+    },
     draw: () => {
       ctx.fillStyle = 'white'
       ctx.fillRect(self.x, self.y, paddleWidth, paddleLength)
     },
-    update: (velocity) => {
+    update: () => {
       self.draw()
-      self.y += velocity
+      if (self.y + self.velocity > 0 && self.y + self.velocity + paddleLength < window.innerHeight)
+        self.y += self.velocity
     },
   }
   return {
@@ -62,7 +73,7 @@ const player2 = paddleFactory({ x: window.innerWidth - 10 * 2, y: 100 })
 const animate = () => {
   requestAnimationFrame(animate)
   ctx.clearRect(0, 0, innerWidth, innerHeight)
-  player1.self.draw()
-  player2.self.draw()
+  player1.self.update()
+  player2.self.update()
 }
 animate()
