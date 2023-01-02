d9b23e11762ebab30c6df007069a7c926ecc1a41
Neil Douglas <endy64@icloud.com>
2023-01-02 14:42:37 +0000

    ball

--- script.js
+++ script.js
@@ -55,25 +55,39 @@ const paddleFactory = ({ x, y }) => {
   }
   return {
     self,
-    // draw: () => {
-    //   ctx.fillStyle = 'white'
-    //   ctx.fillRect(self.x, self.y, paddleWidth, paddleLength)
-    // },
-    // update: (velocity) => {
-    //   self.y += velocity
-    //   ctx.fillStyle = 'white'
-    //   ctx.fillRect(self.x, self.y, paddleWidth, paddleLength)
-    // },
+  }
+}
+
+const ballFactory = ({ x, y, velocityX, velocityY }) => {
+  let self = {
+    x,
+    y,
+    velocityX,
+    velocityY,
+    draw: () => {
+      ctx.fillStyle = 'white'
+      ctx.fillRect(self.x, self.y, 10, 10)
+    },
+    update: () => {
+      self.draw()
+      if (self.y + self.velocity > 0 && self.y + self.velocity + 10 < window.innerHeight)
+        self.y += self.velocity
+    },
+  }
+  return {
+    self,
   }
 }
 
 const player1 = paddleFactory({ x: 10, y: 100 })
 const player2 = paddleFactory({ x: window.innerWidth - 10 * 2, y: 100 })
+const ball = ballFactory({ x: canvas.height / 2, y: canvas.width / 2, velocityX: 1, velocityY: 1 })
 
 const animate = () => {
   requestAnimationFrame(animate)
   ctx.clearRect(0, 0, innerWidth, innerHeight)
   player1.self.update()
   player2.self.update()
+  ball.self.update()
 }
 animate()
