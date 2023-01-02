2715335d37b1f71e721a191ec3d151899242d8c4
Neil Douglas <endy64@icloud.com>
2023-01-02 20:42:10 +0000

    ball spawns randomly

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let ctx = canvas.getContext('2d')
 const paddleLength = 150
 const paddleWidth = 10
 const paddleSpeed = 6
+const ballSpeed = 20
 
 window.addEventListener('keydown', (e) => {
   switch (e.key) {
@@ -70,8 +71,16 @@ const ballFactory = ({ x, y, velocityX, velocityY }) => {
     },
     update: () => {
       self.draw()
-      if (self.y + self.velocity > 0 && self.y + self.velocity + 10 < window.innerHeight)
-        self.y += self.velocity
+      if (self.y + self.velocityY > 0 && self.y + self.velocityY + 10 < window.innerHeight)
+        self.y += self.velocityY
+      else {
+        self.velocityY = -self.velocityY
+      }
+      if (self.x + self.velocityX > 0 && self.x + self.velocityX + 10 < window.innerWidth)
+        self.x += self.velocityX
+      else {
+        self.velocityX = -self.velocityX
+      }
     },
   }
   return {
@@ -79,9 +88,33 @@ const ballFactory = ({ x, y, velocityX, velocityY }) => {
   }
 }
 
+const randomDirection = () => {
+  // let random = Math.random()
+  // if (random < 0.05) random *= 10
+  // random - 0.5
+  // console.log(random)
+  // return random
+  return Math.random() - 0.5
+}
+
+const coinFlip = () => {
+  let coin = Math.random()
+  if (coin < 0.5) return -0.5 * ballSpeed
+  else {
+    return 0.5 * ballSpeed
+  }
+}
+
 const player1 = paddleFactory({ x: 10, y: 100 })
 const player2 = paddleFactory({ x: window.innerWidth - 10 * 2, y: 100 })
-const ball = ballFactory({ x: canvas.height / 2, y: canvas.width / 2, velocityX: 1, velocityY: 1 })
+const ball = ballFactory({
+  x: window.innerWidth / 2,
+  y: window.innerHeight / 2,
+  // velocityX: randomDirection() * ballSpeed,
+  // randomly pick a left or right direction, but ensure it's consistent speed
+  velocityX: coinFlip(),
+  velocityY: randomDirection() * (ballSpeed / 2),
+})
 
 const animate = () => {
   requestAnimationFrame(animate)
